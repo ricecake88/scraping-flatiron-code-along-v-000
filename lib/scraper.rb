843648bf3a6a9133c1e76464f264449fd90271f7
Grace Shih <vocalise@gmail.com>
2018-05-19 04:16:13 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,8 @@ class Scraper
     def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
       binding.pry
+      @all = doc.css("post")
+      @all.each do |
     end
     
     def get_courses
